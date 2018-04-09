d42f17b0234cb4916d0c5767d1a1fbc09f73cd64
gperez13 <giovanni_perez4@yahoo.com>
2018-04-08 21:31:33 -0500

    Binary Search Tree: Need to go deeper on this type of problem

---
+++ Reps/BinarySearchTree.js
@@ -0,0 +1,18 @@
+
+data = [1,2,3,4,5,6,7,8,9]
+
+class Bool {
+	data;
+	Bool.left;
+	Bool.right
+
+}
+
+
+if(b === null){
+	return false;
+} else if( Bool.val < v){
+	return Bool.left;
+} else{
+	return Bool.right
+}

8469d93af89780963a34fa0a35a99a842cd97493
gperez13 <giovanni_perez4@yahoo.com>
2018-06-13 12:06:57 -0500

    Added Socket.io into React app and created initial component. Will work on communication functions

---
+++ ReactApps/sockets/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Layout from './Components/layout';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Layout title="Chat App Bby"/>
+      </div>
+    );
+  }
+}
+
+export default App;

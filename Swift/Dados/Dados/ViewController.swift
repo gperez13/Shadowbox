bc8f60b4b2fc17aa29b8d465c388960d475c1e60
gperez13 <giovanni_perez4@yahoo.com>
2018-06-19 12:41:00 -0500

    Added Dice randomizer functionality

--- Swift/Dados/Dados/ViewController.swift
+++ Swift/Dados/Dados/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    var diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomizeDieImages()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +30,21 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomizeDieImages()
+
+
+    }
+    
+    func randomizeDieImages () {
+        
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+        
+    }
 }
 
